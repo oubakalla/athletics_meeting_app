bd0057746ca4a6d89d5fd5dc078b9ac765be63c7
Imad El Azyfy <imadelazyfy@gmail.com>
2019-01-07 19:15:11 +0100

    some modification in the navigation menu text content

--- index.php
+++ index.php
@@ -27,23 +27,19 @@
                         </a>
                     </li>
                     <li>
-                        <a href="#">add athlete</a>
+                        <a href="#">Add athlete</a>
                     </li>
                     <li>
-                        <a href="#">update athlete</a>
+                        <a href="#">Update athlete</a>
                     </li>
                     <li>
-                        <a href="#">athletes list</a>
+                        <a href="#">Athletes list</a>
                     </li>
                     <li class="dropdown">
-                        <a href="#" class="dropdown-toggle" data-toggle="dropdown">admitted/rejected athletes <span class="caret"></span></a>
+                        <a href="#" class="dropdown-toggle" data-toggle="dropdown">Athletes<span class="caret"></span></a>
                         <ul class="dropdown-menu" role="menu">
-                            <li class="dropdown-header">Dropdown heading</li>
-                            <li><a href="#">Action</a></li>
-                            <li><a href="#">Another action</a></li>
-                            <li><a href="#">Something else here</a></li>
-                            <li><a href="#">Separated link</a></li>
-                            <li><a href="#">One more separated link</a></li>
+                            <li><a href="#">Addmitted</a></li>
+                            <li><a href="#">Rejected</a></li>
                         </ul>
                     </li>
                     <li>
