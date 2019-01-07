58d1ba9b2e29138599b6b38f8e61916047ed5fec
Imad El Azyfy <imadelazyfy@gmail.com>
2019-01-07 01:45:01 +0100

    basic files/folders and libraries structure

---
+++ index.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title>Athletics Meeting App</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="assets/bootstrap-3.4.0-dist/css/bootstrap.min.css">
+    <link rel="stylesheet" type="text/css" media="screen" href="main.css" />
+</head>
+<body>
+    <!-- <?php
+    include 'functions.php';
+    ?> -->
+    <div class="container">
+        <header class="row jumbotron">
+            <nav class="col-xs-1">
+                <ul>
+                    <li></li>
+                </ul>
+            </nav>
+            <h1 class="col-xs-11">Athletics Meeting</h1>
+        </header>
+    </div>
+    <script src="assets/jquery-3.3.1.min.js"></script>
+    <script src="main.js"></script>
+</body>
+</html>
